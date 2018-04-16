ff4e34bd1712d2ab1e45e3a18644057ab74e6829
Matteo <fundor333@gmail.com>
2018-04-16 23:15:04 +0200

    Add the base for the agency model

--- django-transit/models.py
+++ django-transit/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 
-class MyModel(models.Model):
-    name = models.CharField(max_length=255)
+class Agency(models.Model):
+    agency_id = models.CharField(
+        max_length=255, blank=True, null=True, unique=True)
+    agency_name = models.CharField(max_length=400)
+    agency_url = models.URLField()
+    agency_timezone = models.CharField(max_length=20)
+    agency_lang = models.CharField(max_length=2, null=True, blank=True)
+    agency_phone = models.CharField(max_length=20, null=True, blank=True)
+    agency_fare_url = models.URLField(null=True, blank=True)
+    agency_email = models.EmailField(null=True, blank=True)
