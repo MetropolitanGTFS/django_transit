129e9596f3474ebbeed7488070c3aaf0dbaef619
Matteo <fundor333@gmail.com>
2018-05-23 19:31:06 +0200

    Sistemazione dei trips

--- django-transit/models/trips.py
+++ django-transit/models/trips.py
@@ -22,13 +22,13 @@ BIKES_ALLOWED_FIELD = {
 class Trip(models.Model):
     route_id = models.ForeignKeyField(
         'Route', related_name='route_id', on_delete=models.CASCADE)
-    service_id = CharField(max_lenght=300, unique=True)
-    trip_id = CharField(max_lenght=300, primary_key=True)
+    service_id = CharField(max_length=300, unique=True)
+    trip_id = CharField(max_length=300, primary_key=True)
     trip_headsign = CharField(max_lengh=300, blank=True, null=True)
-    trip_short_name = CharField(max_lengh=200, blank=True, null=True)
+    trip_short_name = CharField(max_length=200, blank=True, null=True)
     direction_id = IntegerField(
         choices=DIRECTION_ID_FIELD, blank=True, null=True)
-    block_id = CharField(max_lenght=50, blank=True, null=True)
+    block_id = CharField(max_length=50, blank=True, null=True)
     wheelchair_accessible = IntegerField(
         choices=WHEELCHAIR_ACCESSIBLE_FIELD, default=0)
     bikes_allowed = IntegerField(choices=BIKES_ALLOWED_FIELD, default=0)
